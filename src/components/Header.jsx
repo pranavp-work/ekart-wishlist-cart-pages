00842685107b63d458b91ea88800df120b115d90
pranavp-work <pranavp.work@gmail.com>
2024-12-09 04:37:08 +0530

    day 1 --tooLate header & footer completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,53 @@
-import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <nav className='bg-violet-900 p-5 text-slate-50 w-full'>
-        <div className="flex item-center px-10">
+        <div className="md:flex item-center md:px-10 px-5">
 
-          <Link to = {'/'}>
-            <h1 className='text-3xl'><FontAwesomeIcon icon={faCartShopping} /> Ekart</h1>
-          </Link>
+          <div className='flex w-full'>
+            <Link to = {'/'}>
+              <h1 className='text-3xl'><FontAwesomeIcon icon={faCartShopping} /> Ekart</h1>
+            </Link>
 
-          <ul className='ms-auto flex'>
+            <div className='ms-auto md:hidden'>
+              <button onClick={ () => setShow(!show) }><FontAwesomeIcon icon={faBars} className='text-slate-50 border border-slate-50 p-3 rounded' /></button>
+            </div>
+          </div>
+          
+          { show &&  
+            <ul className='ms-auto md:hidden flex'>
+              <li>
+                <Link to = {'/wishlist'}>
+                  <button className='border border-slate-50 p-3 rounded hover:bg-slate-50 hover:text-black flex'><FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",}} className='me-2' />Wishlist<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
+                </Link>
+              </li>
+
+              <li>
+                <Link to = {'/cart'}>
+                  <button className='border border-slate-50 p-3 rounded ms-3 hover:bg-slate-50 hover:text-black flex'><FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff00",}} className='me-2' />Cart<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
+                </Link>
+              </li>
+            </ul>
+          }
+
+          <ul className='ms-auto md:flex hidden'>
             <li>
               <Link to = {'/wishlist'}>
-                <button className='border border-slate-50 p-3 rounded hover:bg-slate-50 hover:text-black'><FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",}} className='me-2' />Wishlist<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
+                <button className='border border-slate-50 p-3 rounded hover:bg-slate-50 hover:text-black flex'><FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",}} className='me-2' />Wishlist<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
               </Link>
             </li>
 
             <li>
               <Link to = {'/cart'}>
-                <button className='border border-slate-50 p-3 rounded ms-3 hover:bg-slate-50 hover:text-black'><FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff00",}} className='me-2' />Cart<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
+                <button className='border border-slate-50 p-3 rounded ms-3 hover:bg-slate-50 hover:text-black flex'><FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff00",}} className='me-2' />Cart<span className='rounded border border-slate-50 bg-slate-50 text-black ms-2'>0</span></button>
               </Link>
             </li>
           </ul>
